3345701a202b3ff8df268ba281723ec17e71ef54
nielinjie <nielinjie@gmail.com>
2021-05-25 22:30:31 +0800

    product server

--- run-product.js
+++ run-product.js
@@ -1,10 +1,12 @@
+console.log(`starting product server for front3`);
+
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.use(express.static("build"));
-const setP = require("setupProxy");
-setP(app)
+const setP = require("src/setupProxy");
+setP(app);
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`server started, listening at http://localhost:${port}`);
 });
