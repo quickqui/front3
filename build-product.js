9056f5ee202f1ce012e973cd31685effdfaf309f
nielinjie <nielinjie@gmail.com>
2021-05-29 18:09:50 +0800

    build debug

--- build-product.js
+++ build-product.js
@@ -1,4 +1,16 @@
-const distPath = process.env.DIST_PATH
-const { execSync } = require("child_process");
-execSync("npm run build");
-execSync('mv build '+distPath)
+const distPath = process.env.DIST_PATH;
+const path = require("path");
+const { spawnSync } = require("child_process");
+const cwd = path.resolve(__dirname);
+console.log(`cwd`, cwd);
+execS("npm", ["run", "build"], cwd);
+console.log("distPath  - " + distPath);
+execS("mv", ["build", distPath], cwd);
+
+function execS(command, args, cwd) {
+  spawnSync(command, args, {
+    cwd: cwd ? cwd : path.resolve(process.cwd()),
+    stdio: "inherit",
+    // env: { ...process.env, ...env },
+  });
+}
