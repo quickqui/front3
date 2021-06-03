7fd67a755755d8583c7714159c72e3321adb7bd9
nielinjie <nielinjie@gmail.com>
2021-06-03 20:25:49 +0800

    build

--- build-product.js
+++ build-product.js
@@ -1,10 +1,12 @@
 const distPath = process.env.DIST_PATH;
 const path = require("path");
+const fs = require("fs");
 const { spawnSync } = require("child_process");
 const cwd = path.resolve(__dirname);
 console.log(`cwd`, cwd);
 execS("npm", ["run", "build"], cwd);
 console.log("distPath  - " + distPath);
+fs.rmdirSync(path.resolve(distPath, "build"), { recursive: true });
 execS("mv", ["build", distPath], cwd);
 
 function execS(command, args, cwd) {
