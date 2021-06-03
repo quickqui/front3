943508cf7485d812c78f5aaf362d3265f9a33764
nielinjie <nielinjie@gmail.com>
2021-06-03 20:05:09 +0800

    buildpath

--- run-product.js
+++ run-product.js
@@ -1,10 +1,14 @@
+const process = require("process");
 console.log(`starting product server for front3`);
 
 const express = require("express");
 const app = express();
-const port = 3000;
-
-app.use(express.static("build"));
+//from env?
+const port = process.env.PORT ? 0 + process.env.PORT : 3000;
+const builtPath = process.env.BUILT_PATH;
+console.log("port :>> ", port);
+console.log(`builtPath`, builtPath);
+app.use(express.static(builtPath));
 const setP = require("./src/setupProxy");
 setP(app);
 app.listen(port, () => {
